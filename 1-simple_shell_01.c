bcd4ad2f3c89fb10b7809cc8abe0f3aa6ca6f1f5
SirLancelotTheNerdy <lancelot.gay@atlasschool.com>
2024-07-19 15:13:52 -0500

    Modified the user input

--- 1-simple_shell_01.c
+++ 1-simple_shell_01.c
@@ -6,14 +6,11 @@
 
 int main(void) {
     int input;
-    int i; 
-    i = 1;
-while (i < 0 )
+
     printf("Enter Comand: ");
     scanf("%s", input);
 
     /* input to refrence the comand.*/ 
     printf("you entered %s" , input);
-    i++;
     return (0);
     }
