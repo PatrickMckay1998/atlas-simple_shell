c7da5c524be107f3f97caece75f23e2336cc49b8
SirLancelotTheNerdy <lancelot.gay@atlasschool.com>
2024-07-19 15:02:31 -0500

    test

---
+++ test_functions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.c>
+
+int main() 
+{
+    int a, b;
+   
+      printf("Enter first number: ");
+      scanf("%d", &a);
+   
+      printf("Enter second number: ");
+      scanf("%d", &b);
+   
+      printf("A : %d \t B : %d" , 
+            a , b);
+   
+    return 0;
+}
+/*test*/
